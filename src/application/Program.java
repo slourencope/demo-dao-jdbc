7a4e1e43afc8ddb8be4d39e14fac8465ceecbeb2
slourencope <slourencope@hotmail.com>
2020-04-02 22:21:23 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,9 @@
  */
 package application;
 
+import java.util.Date;
 import model.entities.Departament;
+import model.entities.Seller;
 
 /**
  *
@@ -20,6 +22,8 @@ public class Program {
         // TODO code application logic here
         Departament obj = new Departament(1, "Books");
         System.out.println(obj);
+        
+        Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.00, obj);
+        System.out.println(seller);
     }
-    
 }

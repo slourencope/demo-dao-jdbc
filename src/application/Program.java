5a2eab649393f01bd7b262ba1beb3d23a820b219
slourencope <slourencope@hotmail.com>
2020-04-10 13:03:45 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,8 +25,12 @@ public class Program {
         Departament obj = new Departament(1, "Books");
         System.out.println(obj);
         
-        Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.00, obj);
+        //Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.00, obj);
+        
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        
+        Seller seller = sellerDao.findById(3);
+        
         System.out.println(seller);
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -5,8 +5,17 @@
  */
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.dao.SellerDao;
+import model.entities.Departament;
 import model.entities.Seller;
 
 /**
@@ -15,6 +24,12 @@ import model.entities.Seller;
  */
 public class SellerDaoJDBC implements SellerDao {
 
+    private Connection conn;
+    
+    public SellerDaoJDBC (Connection conn){
+        this.conn = conn;
+    }
+    
     @Override
     public void insert(Seller obj) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -32,7 +47,37 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                "SELECT seller.*,department.Name as DepName "
+                + "FROM seller INNER JOIN department "
+                + "ON seller.DepartmentId = department.Id "
+                + "WHERE seller.Id = ?");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()){
+                Departament dep = new Departament();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+                Seller obj = new Seller();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                obj.setEmail(rs.getString("Email"));
+                obj.setBaseSalary(rs.getDouble("BaseSalary"));
+                obj.setBirthDate(rs.getDate("BirthDate"));
+                obj.setDepartament(dep);
+                return obj;
+            }
+            return null;
+        } catch (SQLException ex) {
+            throw new DbException(ex.getMessage());
+            //Logger.getLogger(SellerDaoJDBC.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
